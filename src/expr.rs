eb02be591a97f4c07538f29e434829c69316d6e6
Thomas <thomas.terry@sierra-lang.net>
2022-03-25 17:50:38 -0500

    Do more function extration, in eval this time

--- src/expr.rs
+++ src/expr.rs
@@ -49,41 +49,46 @@ impl LispExpr {
     pub fn eval(&self) -> Result<LispExpr> {
         match self {
             // Symbols and numbers both are in their final form for now
+            //
             // TODO: In the future we need to look up symbols in the environment and replace them
             // with their value
-            Self::Symbol(_) | Self::Number(_) => Ok(self.clone()),
-
+            //
             // A List has to be actually evaluated
-            Self::List(list) => {
-                // We need to check if we have anything in the list so we can apply the function
-                // Some functions may not take any arguments but they at least have a name
-                let (f, args) = if let Some((f, args)) = list.split_first() {
-                    (f, args)
-                } else {
-                    return Err(LispError::reason(
-                        "Expected a function application got empty list",
-                    ));
-                };
+            Self::List(list) => Self::eval_list(list),
+            _ => Ok(self.clone()),
+        }
+    }
 
-                match f {
-                    Self::Symbol(name) => {
-                        match name.as_str() {
-                            // Just the add function for now
-                            "+" => add(args),
-                            "-" => sub(args),
-                            _ => {
-                                // There is no function with that name
-                                Err(LispError::reason(format!("Unknown function {}", name)))
-                            }
-                        }
+    fn eval_list(list: &[LispExpr]) -> Result<LispExpr> {
+        // We need to check if we have anything in the list so we can apply the function
+        // Some functions may not take any arguments but they at least have a name
+        let (f, args) = if let Some((f, args)) = list.split_first() {
+            (f, args)
+        } else {
+            return Err(LispError::reason(
+                "Expected a function application got empty list",
+            ));
+        };
+
+        match f {
+            Self::Symbol(name) => {
+                match name.as_str() {
+                    // Just the add function for now
+                    "+" => add(args),
+                    "-" => sub(args),
+                    _ => {
+                        // There is no function with that name
+                        Err(LispError::reason(format!("Unknown function {}", name)))
                     }
-                    // We didn't get a function to apply
-                    _ => Err(LispError::reason(format!(
-                        "Expected a function name, got {:?}",
-                        self
-                    ))),
                 }
             }
+            // We didn't get a function to apply
+            // TODO: in future function could be a lambda which will not be just a function name in
+            // a lookup
+            _ => Err(LispError::reason(format!(
+                "Expected a function name, got {:?}",
+                list
+            ))),
         }
     }
 }
