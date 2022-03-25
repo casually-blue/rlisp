720fc26467010e984604601038c7abc51f5fa6b9
Thomas <thomas.terry@sierra-lang.net>
2022-03-25 17:26:54 -0500

    Extract functions

--- src/expr.rs
+++ src/expr.rs
@@ -8,6 +8,43 @@ pub enum LispExpr {
     List(Vec<LispExpr>),
 }
 
+pub fn add(args: &[LispExpr]) -> Result<LispExpr> {
+    // Create a variable to accumulate the result
+    let mut result: f64 = 0.0;
+    // Iterate over each arg and do something with it
+    for arg in args {
+        result += match arg.eval()? {
+            // We have just a number so we can add it to the accumulator
+            // directly
+            LispExpr::Number(n) => n,
+            // We probably have a function
+            // We didn't get a number so we just error
+            _ => return Err(LispError::reason("Expected a number")),
+        }
+    }
+    // Return the result
+    Ok(LispExpr::Number(result))
+}
+
+pub fn sub(args: &[LispExpr]) -> Result<LispExpr> {
+    match args.len() {
+        2 => {
+            // We need two numbers and we know we have only two arguments
+            // so we can just force unwrap them from the option
+            let (x, xs) = args.split_first().unwrap();
+            let (y, _) = xs.split_first().unwrap();
+
+            // Call eval on both so that we have their applied form if they are
+            // function calls
+            match (x.eval()?, y.eval()?) {
+                (LispExpr::Number(a), LispExpr::Number(b)) => Ok(LispExpr::Number(a - b)),
+                _ => Err(LispError::reason("Both arguments must be numbers")),
+            }
+        }
+        _ => Err(LispError::reason("Subtract only takes two arguments")),
+    }
+}
+
 impl LispExpr {
     pub fn eval(&self) -> Result<LispExpr> {
         match self {
@@ -30,69 +67,16 @@ impl LispExpr {
                     Self::Symbol(name) => {
                         match name.as_str() {
                             // Just the add function for now
-                            "+" => {
-                                // Create a variable to accumulate the result
-                                let mut result: f64 = 0.0;
-                                // Iterate over each arg and do something with it
-                                for arg in args {
-                                    result += match arg {
-                                        // We have just a number so we can add it to the accumulator
-                                        // directly
-                                        Self::Number(n) => *n,
-                                        // We probably have a function
-                                        Self::List(_) => {
-                                            match arg.eval()? {
-                                                // If it evaluates to a number we just add it to the
-                                                // accumulator
-                                                Self::Number(n) => n,
-                                                // The result of evaluating the function was wrongly
-                                                // typed
-                                                _ => {
-                                                    return Err(LispError::reason(
-                                                        "Wrong type of argument",
-                                                    ))
-                                                }
-                                            }
-                                        }
-                                        // We didn't get a number so we just error
-                                        _ => return Err(LispError::reason("Expected a number")),
-                                    }
-                                }
-                                // Return the result
-                                Ok(Self::Number(result))
-                            }
-                            "-" => {
-                                match args.len() {
-                                    2 => {
-                                        // We need two numbers and we know we have only two arguments
-                                        // so we can just force unwrap them from the option
-                                        let a = args.first().unwrap();
-                                        let b = args.iter().skip(1).next().unwrap();
-
-                                        // Call eval on both so that we have their applied form if they are
-                                        // function calls
-                                        match (a.eval()?, b.eval()?) {
-                                            (Self::Number(a), Self::Number(b)) => {
-                                                Ok(Self::Number(a - b))
-                                            }
-                                            _ => Err(LispError::reason(
-                                                "Both arguments must be numbers",
-                                            )),
-                                        }
-                                    }
-                                    _ => {
-                                        Err(LispError::reason("Subtract only takes two arguments"))
-                                    }
-                                }
-                            }
+                            "+" => add(args),
+                            "-" => sub(args),
                             _ => {
                                 // There is no function with that name
-                                Err(LispError::reason("Unknown function"))
+                                Err(LispError::reason(format!("Unknown function {}", name)))
                             }
                         }
                     }
                     // We didn't get a function to apply
-                    _ => Err(LispError::reason("Expected a function name")),
+                    _ => Err(LispError::reason(format!("Expected a function name, got {:?}", self))),
                 }
             }
         }
